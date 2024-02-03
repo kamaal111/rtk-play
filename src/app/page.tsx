7ad545047036d788666e37ea3fb32d2283a6c165
Kamaal Farah <kamaal.f1@gmail.com>
2024-02-03 23:48:09 +0000

    Adding back todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import React from "react";
 
 import MainLayout from "@/components/main-layout";
 import TodoForm from "@/features/todos/components/todo-form";
+import TodoList from "@/features/todos/components/todo-list";
 
 function Home() {
   return (
     <MainLayout title="Todos">
       <TodoForm />
+      <TodoList />
     </MainLayout>
   );
 }
